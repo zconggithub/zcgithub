b124e09871b66e5fc531b4b31d8ebf3c4a8239af
周聪 <@zc>
2017-06-14 01:12:45 +0800

    家里第二次修改提交

--- springboot-log4j/src/main/java/com/example/demo/controller/DemoController.java
+++ springboot-log4j/src/main/java/com/example/demo/controller/DemoController.java
@@ -23,6 +23,7 @@ public class DemoController {
 	logger.info("####log4j的测试####");
 		logger.info("####第一次测试####");
 		logger.info("####我是家用电脑####,master更改");
+		logger.info("####我是家用电脑第二次修改测试并提交####,master更改");
 		return friend;
 	}
 	
