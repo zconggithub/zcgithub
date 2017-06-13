a41672ce38aed56d27bf258359701bd7b206ff27
周聪 <@zc>
2017-06-14 01:09:48 +0800

    oo

--- springboot-log4j/src/main/java/com/example/demo/controller/DemoController.java
+++ springboot-log4j/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,4 +1,4 @@
-,package com.example.demo.controller;
+package com.example.demo.controller;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
