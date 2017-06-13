24c8a4a82547c2faf8a05175594ea0e833c374dc
zconggithub <zc2632652610@sina.com>
2017-06-14 01:19:08 +0800

    Create DemoController.java

--- springboot-log4j/src/main/java/com/example/demo/controller/DemoController.java
+++ springboot-log4j/src/main/java/com/example/demo/controller/DemoController.java
@@ -22,7 +22,7 @@ public class DemoController {
 	Friend friend=FriendService.selectByPrimaryKey(1);
 	logger.info("####log4j的测试####");
 		logger.info("####第一次测试####");
-		logger.info("####我是家用电脑####");
+		logger.info("####原始master####");
 		return friend;
 	}
 	
